656b2238ff438a59fd05307c8ad717a4ebe8fc27
Near <weerawich_won@student.mahidol.edu>
2022-10-23 15:17:13 +0700

    update with docker

--- flask/env/Scripts/activate.bat
+++ flask/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Near\Desktop\Final_project\docker-flask\flask\env
+set VIRTUAL_ENV=C:\Users\Near\Desktop\Final_project\server-test\app\env
 
 if not defined PROMPT set PROMPT=$P$G
 
